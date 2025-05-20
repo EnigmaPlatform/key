c49b0c9dc6e88edf6b78ee0c8328d1ac5dcbaddc
Millennium <71434392+EnigmaPlatform@users.noreply.github.com>
2025-05-20 13:47:39 +1100

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ CONFIG = {
     'FOUND_KEYS_FILE': "found_keys.txt",
     'CHUNK_SIZE': 10_000_000,
     'MIN_CHUNK_SIZE': 1,
-    'MAIN_START': 0x349b84b6431a614ef1,
+    'MAIN_START': 0x349b84b64311614ef1,
     'MAIN_END': 0x349b84b6431a6c4ef1,
     'BATCH_SIZE': 1_000_000,
     'MAX_WORKERS': 12,
