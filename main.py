caac39e7c2202bd36491e24e5722af3d9e6d24f1
Millennium <71434392+EnigmaPlatform@users.noreply.github.com>
2025-05-23 11:17:22 +1100

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from functools import lru_cache
 class Config:
     FOUND_FILE = "found.txt"
     TARGET = bytes.fromhex("5db8cda53a6a002db10365967d7f85d19e171b10")  # Замените на нужный хеш
-    START = 0x349b84b6431a6c4ef1  # Начальный ключ
-    END = 0x349b84b6431a5c4ef1    # Конечный ключ
+    START = 0x349b84b6431a4c4ef1  # Начальный ключ
+    END = 0x349b84b6431a6c4ef1    # Конечный ключ
     BATCH = 5_000             # Размер батча на ядро
     UPDATE = 1_000_000            # Частота обновления статуса
 
