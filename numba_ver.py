d1a68c5357e9a841db65c1c3a4860386640356e9
Millennium <71434392+EnigmaPlatform@users.noreply.github.com>
2025-05-27 14:41:16 +1100

    Update numba_ver.py

--- numba_ver.py
+++ numba_ver.py
@@ -139,7 +139,7 @@ def run_tests():
     test_cases = [
         ("0000000000000000000000000000000000000000000000000000000000000000", True),
         ("abcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcd", True),
-        ("a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a", False)
+        ("a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a", True)
     ]
     
     try:
